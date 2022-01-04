8061c6a267a56fe72782334d3c4c6318a376dfb7
Justsnoopy30 <justsnoopy30@hypercubemc.tk>
2022-01-04 17:05:39 -0600

    Adjust the allowed sodium versions for starline and add starline-specific info to the incompatible sodium version screen

--- src/main/java/net/coderbot/iris/compat/sodium/SodiumVersionCheck.java
+++ src/main/java/net/coderbot/iris/compat/sodium/SodiumVersionCheck.java
@@ -5,14 +5,14 @@ import com.google.common.collect.ImmutableList;
 public class SodiumVersionCheck {
 	// The allowed versions of Sodium for use with Iris
 	private static final ImmutableList<AllowedSodiumVersion> ALLOWED_SODIUM_VERSIONS = ImmutableList.of(
-			// Official 0.4.0-alpha5
-			AllowedSodiumVersion.exact("0.4.0-alpha5+build.9"),
+			// Sodium 1.18.1 version with proper fog, actions build
+			AllowedSodiumVersion.exact("0.4.0-alpha5+build.816"),
 
-			// ReplayMod's existing compatible forked 0.4.0-alpha5 version
-			AllowedSodiumVersion.prefix("0.4.0-alpha5+rev.76d0e6e"),
+			// Sodium 1.18.1 version with proper fog found on our discord
+			AllowedSodiumVersion.exact("0.4.0-alpha5+rev.63aafcd"),
 
-			// For future use by ReplayMod
-			AllowedSodiumVersion.prefix("0.4.0-alpha5+replaymod")
+			// Custom Sodium 1.18.1 version used in Universe with FREX Flawless Frames support (needed for ReplayMod render exporting with sodium)
+			AllowedSodiumVersion.exact("0.4.0-alpha5+rev.d3a2a28")
 	);
 
 	public static boolean isAllowedVersion(String sodiumVersion) {
